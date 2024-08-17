e8d3ac7430ace4bf0e81a66d064ec6e180dbf32b
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-08-17 12:09:53 -0300

    feat: change error format for data validation

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine
+from fastapi.exceptions import RequestValidationError
+
+from app.routes.user_router import user_routes
 
 app = FastAPI()
 
 engine = create_engine("sqlite:///database.db")
+
+app.include_router(user_routes)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exception: RequestValidationError):
+    details = exception.errors()
+
+    for error in details:
+        if error.get("type") == "missing":
+            return JSONResponse(
+                content={ "Message_error": "Todos os dados são necessários." },
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+        else:
+            return JSONResponse(
+                content={ "Message_error": "Dados inválidos, verificar tipos de dados." },
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
 
 if __name__ == "__main__":
     import uvicorn
